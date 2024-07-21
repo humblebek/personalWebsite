39a8cf712db825323ab9dc930347f625a56a835c
humblebek <your-email@example.com>
2024-07-21 12:57:21 +0500

    Disable Sign up

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>devazizbek</title>
+    <title>😊devazizbek</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://kit.fontawesome.com/4115797fc9.js" crossorigin="anonymous"></script>
